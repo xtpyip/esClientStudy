7782f6761dce03e55460cf3e6430fb201a36b719
xtpyip <pyip2001@qq.com>
2024-06-04 15:21:01 +0800

    框架

---
+++ hotel/src/main/java/com/pyip/hotel/mapper/HotelMapper.java
@@ -0,0 +1,8 @@
+package com.pyip.hotel.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pyip.hotel.pojo.Hotel;
+
+public interface HotelMapper extends BaseMapper<Hotel> {
+}
---
+++ hotel/src/main/java/com/pyip/hotel/service/IHotelService.java
@@ -0,0 +1,7 @@
+package com.pyip.hotel.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.pyip.hotel.pojo.Hotel;
+
+public interface IHotelService extends IService<Hotel> {
+}
